6a105337b3db9b303c8b9746dc31db819bbe76d9
hs2361 <sharmaharsh@me.com>
2020-02-24 00:59:00 +0530

    Added comments and formatting

--- db/database.js
+++ db/database.js
@@ -1,15 +1,15 @@
-const mysql = require("mysql");
+const mysql = require("mysql"); //import module
 
-const mySqlConnection = mysql.createConnection({
+const mySqlConnection = mysql.createConnection({ //database credentials
     host: "localhost",
     user: "webkriti_server",
     password: "CPimfa!2306",
     database: "webkriti"
 });
 
-mySqlConnection.connect(err => {
+mySqlConnection.connect(err => { //connect to database
     if (err) console.log(err);
     else console.log("Database Connected!");
 });
 
-module.exports = mySqlConnection;
+module.exports = mySqlConnection; //export connection
