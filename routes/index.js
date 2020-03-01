e75fa9c0e8f378e5502f061ba03e2aeb63c7f931
Aaryak Shah <aaryakdshah@gmail.com>
2020-03-01 11:02:06 +0530

    corrections to index.js

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,15 @@ const mySqlConnection = require("../db/database");
 
 router.get('/', (req, res) => res.status(200).send('home-page')); //home page
 
+/*-----------------------------------------------------------------------------------*/
+//restaurant dashboard backend:
+
 //get request for dashboard, will ask for login if cookie is not found
 router.get('/rdashboard', (req, res) => 
 {
-    if (req.session.user) {
+    if (req.session.user.rid) {
         mySqlConnection.query(
-            `select * from orders where rid = ${req.session.user.rid} and status = "nd"`,
+            `select * from orders where rid = ${req.session.user.rid} and delivered = 0`,
             [],
             (err, rows) => {
                 if(err)
@@ -22,14 +25,14 @@ router.get('/rdashboard', (req, res) =>
         )
     }
     else
-        res.status(401).send('login for this');
+        res.status(401).send('login as restaurant for this');
 });
 
 //get request for rest menu, will simply display menu to restaurant
-router.get('/rdashboard/menu/:rid', (req, res) => {
+router.get('/rdashboard/menu', (req, res) => {
     if(req.session.user.rid) {
         mySqlConnection.query(
-            `select * from menu_${rid}`,
+            `select * from menu_${req.session.user.rid}`,
             [],
             (err, rows) => {
                 if(err) {
@@ -50,7 +53,7 @@ router.get('/rdashboard/menu/:rid', (req, res) => {
 });
 
 //post request for rest menu, for adding new items to menu
-router.post('/rdashboard/menu/:rid', (req, res) => {
+router.post('/rdashboard/menu', (req, res) => {
     if(req.session.user.rid) {
         const { dname, price } = req.body;
         let errors = [];
@@ -59,7 +62,7 @@ router.post('/rdashboard/menu/:rid', (req, res) => {
         }
         
         mySqlConnection.query(
-            `insert into menu_${rid} (dname, price, rating) values (?)`,
+            `insert into menu_${req.session.user.rid} (dname, price, rating) values (?)`,
             [[dname, price, 0]], //setting initial rating to 0
             (err, rows) => {
                 if(err)
@@ -73,7 +76,7 @@ router.post('/rdashboard/menu/:rid', (req, res) => {
         );
     }
     else {
-        res.status(400).send('not a restaurant');
+        res.status(401).send('login as restaurant for this');
     }
 });
 
@@ -90,7 +93,7 @@ router.get('/rdashboard/feedback/:rid', (req, res) => {
                     res.send("No orders yet");
                 }
                 else {
-
+                    res.send(rows);
                 }
             }
         )
@@ -99,5 +102,10 @@ router.get('/rdashboard/feedback/:rid', (req, res) => {
         res.status(401).send('login as restaurant for this');
     }
 })
+
+
+/*-----------------------------------------------------------------------------------*/
+//user dashboard backend:
+
 
 module.exports = router
