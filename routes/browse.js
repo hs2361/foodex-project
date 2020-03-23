a53a9ce0b06e6b53125470f9da3b5227613fb29e
= <=>
2020-03-23 11:59:32 +0530

    Added browse page for each restaurant

--- routes/browse.js
+++ routes/browse.js
@@ -3,14 +3,12 @@ const router = express.Router();
 const mySqlConnection = require("../db/database"); //importing database connection
 
 router.get('/', (req, res) => {
-    if(req.session.user)
-    {
-        if(req.session.user.rid) //check whether is restaurant
+    if (req.session.user) {
+        if (req.session.user.rid) //check whether is restaurant
         {
             res.status(400).send('you must be a user to order'); //bad request
         }
-        else
-        {
+        else {
             const uid = req.session.user.uid;
             mySqlConnection.query( //create cart for user
                 `create table if not exists cart_${uid} (
@@ -18,27 +16,26 @@ router.get('/', (req, res) => {
                     did int
                 );`,
                 [],
-                (err) => 
-                {
-                    if(err)
+                (err) => {
+                    if (err)
                         res.status(500).send(err); //internal server error
                     else {
                         mySqlConnection.query( //shows user a list of restaurants ordered by name
                             `select rid, rname, phone, address,rating from restaurants where verified = true order by rname`,
                             [],
                             (err, rows) => {
-                                if(err)
+                                if (err)
                                     res.status(500).send(err); // internal server error
-                                else if(!rows)
+                                else if (!rows)
                                     res.send('no restaurants to browse'); // no restaurants in catalogue
-                                else 
+                                else
                                     res.status(200).render("browse", {
                                         profile: {
                                             name: req.session.user.uname,
                                             phone: req.session.user.phone,
                                             email: req.session.user.email
                                         },
-                                        data: {rows}
+                                        data: { rows }
                                     }); // dynamically renders list of restaurants for user to browse
                             }
                         );
@@ -52,18 +49,16 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:rid', (req, res) => {
-    if(req.session.user)
-    {
-        if(req.session.user.uid)
-        {
+    if (req.session.user) {
+        if (req.session.user.uid) {
             mySqlConnection.query(
                 `SELECT * FROM restaurants WHERE rid = ${req.params.rid} AND verified = TRUE`,
                 [],
                 (err, rows) => {
-                    if(err) {
+                    if (err) {
                         res.status(500).send(err);
                     }
-                    else if(!rows) {
+                    else if (!rows) {
                         res.status(400).send('Restaurant not found');
                     }
                     else {
@@ -71,29 +66,61 @@ router.get('/:rid', (req, res) => {
                             `SELECT * FROM menu_${req.params.rid}`,
                             [],
                             (e, r) => {
-                                if(e) {
+                                if (e) {
                                     res.status(500).send(e);
                                 }
                                 else {
-                                    res.status(200).render("restaurant", {
-                                        profile: {
-                                            name: req.session.user.uname,
-                                            phone: req.session.user.phone,
-                                            email: req.session.user.email
-                                        },
-                                        data: {rows,r}
-                                    });
+                                    mySqlConnection.query(
+                                        `select did,count(did) as qty from cart_${req.session.user.uid} group by did`,
+                                        [],
+                                        (e2, r2) => {
+                                            if (e2)
+                                                res.status(500).send(e2);
+                                            else {
+                                                var dishes = {};
+                                                var totalPrice = 0;
+
+                                                r2.forEach((el, i) => {
+                                                    mySqlConnection.query(
+                                                        `select *from menu_${req.params.rid} where did = ${el.did}`,
+                                                        [],
+                                                        (e3, r3) => {
+                                                            if (e3)
+                                                                res.status(500).send(e3);
+                                                            else {
+                                                                dishes[r3[0].dname] = [el.qty, r3[0].price * el.qty];
+                                                                totalPrice += r3[0].price * el.qty;
+                                                                if (i == r2.length - 1) {
+                                                                    console.log(dishes);
+                                                                    res.status(200).render("restaurant", {
+                                                                        profile: {
+                                                                            name: req.session.user.uname,
+                                                                            phone: req.session.user.phone,
+                                                                            email: req.session.user.email
+                                                                        },
+                                                                        data: { rows, r, dishes, totalPrice }
+                                                                    })
+                                                                }
+                                                            }
+                                                        }
+                                                    );
+                                                })
+                                            }
+                                        }
+                                    )
                                 }
                             }
-                    )
-                }});
+                        );
+                    }
+                }
+            );
         }
 
         else {
             res.status(401).send("not a user");
         }
     }
-    
+
     else {
         res.redirect('/users/login');
     }
