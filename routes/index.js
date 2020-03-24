38d3e966f60ec21290d7c817e9eb3f2688e58b17
Aaryak Shah <aaryakdshah@gmail.com>
2020-03-24 15:06:05 +0530

    added restaurant dashboard feature and connected to gui

--- routes/index.js
+++ routes/index.js
@@ -11,26 +11,144 @@ router.get('/', (req, res) =>
 //restaurant dashboard backend:
 
 //get request for dashboard, will ask for login if cookie is not found
+
+// order: {
+//     "oid": 1,
+//     "rid": 1,
+//     "rname": "icy spicy",
+//     "otime": "12:00",
+//     "items": {
+//         "d1": 2,
+//         "d2": 1
+//     }
+// }
+
 router.get('/rdashboard', (req, res) => 
 {
-    try {
+    if(req.session.user) {
         if (req.session.user.rid) {
+            let orders = [];
             mySqlConnection.query(
-                `select oid, uid, did, count(did) as qty from orders where rid = ${req.session.user.rid} and delivered = 0 group by oid, did`,
+                `select oid, orders.rid, rname, category, did, delivered, otime, count(did) as qty
+                 from orders, restaurants where orders.rid = ${req.session.user.rid} and orders.rid = restaurants.rid
+                 group by oid,did`,
                 [],
                 (err, rows) => {
                     if(err)
                         res.status(500).send(err);
                     else if(!rows) 
-                        res.send("No running orders ");
-                    else 
-                        res.send(rows);
+                        res.render('rest_dashboard', {check: 'false', o: {}});
+                    else {
+                        let o = {};
+                        o.items = {};
+                        o.amount = 0;
+                        let amount = 0;
+                        let map = {};
+                        rows.forEach((e,i) => { //iterate over all the orders
+                            if(i == 0) //first row
+                            {
+                                o = {}; //make new object for new order
+                                o.oid = e.oid;
+                                o.rid = e.rid;
+                                o.otime = e.otime;
+                                o.rname = e.rname;
+                                o.category = e.category;
+                                o.items = {};
+                                amount = 0;
+                                map[e.oid] = orders.length; //to ensure that orders go into the right object
+                                orders.push(o); //put temp o object into orders array
+                                mySqlConnection.query(
+                                    `select *from menu_${e.rid} where did = ${e.did}`, //get details of dishes
+                                    [],
+                                    (error,r) => {
+                                        if(error)
+                                            res.status(500).send(error)
+                                        else
+                                        {
+                                            orders[0].oid = e.oid;
+                                            orders[0].items[r[0]["dname"]] = e.qty; //set dish name in o.items object
+                                            amount += r[0]["price"] * e.qty; //calculate amount as price * quantity
+                                            orders[0].amount = amount;
+                                        }
+                                    }
+                                );
+                            }
+
+                            else{
+                                if(e.oid == rows[i-1].oid) //for consequent rows of the same order ID
+                                {
+                                    mySqlConnection.query(
+                                        `select *from menu_${e.rid} where did = ${e.did}`, //get details of dishes
+                                        [],
+                                        (error,r) => {
+                                            if(error)
+                                                res.status(500).send(error);
+                                            else
+                                            {
+                                                orders[map[e.oid]].oid = e.oid;
+                                                orders[map[e.oid]].items[r[0]["dname"]] = e.qty;  //set dish name in o.items object
+                                                amount += r[0]["price"] * e.qty; //calculate amount as price * quantity
+                                                orders[map[e.oid]].amount = amount; 
+
+                                                if(i == rows.length - 1) //last row of orders
+                                                {
+                                                    // res.send(orders); //send orders array to user
+                                                    res.render('rest_dashboard', {check: 'true', o: orders});
+                                                }
+                                                return;
+                                            }
+                                        }
+                                    );
+                                }
+
+                                else{
+                                    o = {}; //make new object for new order
+                                    o.oid = e.oid;
+                                    o.rid = e.rid;
+                                    o.otime = e.otime;
+                                    o.rname = e.rname;
+                                    o.category = e.category;
+                                    o.items = {};
+                                    o.amount = 0;
+                                    amount = 0;
+                                    map[e.oid] = orders.length; //to ensure that orders go into the right object
+                                    orders.push(o); //put temp o object into orders array
+
+                                    mySqlConnection.query(
+                                        `select *from menu_${e.rid} where did = ${e.did}`, //get details of dishes
+                                        [],
+                                        (error,r) => {
+                                            if(error)
+                                                res.status(500).send(error)
+                                            else
+                                            {
+                                                orders[map[e.oid]].oid = e.oid;
+                                                orders[map[e.oid]].items[r[0]["dname"]] = e.qty; //set dish name in o.items object
+                                                amount = r[0]["price"] * e.qty; //calculate amount as price * quantity
+                                                orders[map[e.oid]].amount = amount;
+
+                                                if(i == rows.length - 1) //last row of orders
+                                                {
+                                                    // res.send(orders); //send orders array to user
+                                                    res.render('rest_dashboard', {check: 'true', o: orders}); //send orders array to user
+                                                }
+                                                return;
+                                            }
+                                        }
+                                    );
+                                }
+                            }
+                        })
+                    }
                 }
             );
         }
+        else {
+            res.status(401).send('login as restaurant for this');
+        }
     }
-    catch {
-        res.status(401).send('login as restaurant for this');
+    else {
+        res.redirect('/restaurants/login');
     }
 });
 
@@ -111,9 +229,30 @@ router.get('/rdashboard/feedback/', (req, res) => {
     }
 })
 
-
-/*-----------------------------------------------------------------------------------*/
-//user dashboard backend:
+router.get('/rdashboard/accept/:oid', (req, res) => {
+    if(req.session.user) {
+        if(req.session.user.rid) {
+            mySqlConnection.query(
+                `update orders set delivered = 1 where oid = ${req.params.oid}`,
+                [],
+                (err) => {
+                    if(err) {
+                        res.status(500).send(err);
+                    }
+                    else {
+                        res.redirect('/rdashboard');
+                    }
+                }
+            )
+        }
+        else {
+            res.status(401).send('Login as a restaurant for this');
+        }
+    }
+    else {
+        res.redirect('/restaurants/login');
+    }
+})
 
 
 module.exports = router
